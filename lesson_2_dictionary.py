45a1b43a08f3717bd997396f83525466b8edbf0f
andreyka101 <andrejkvasnin236@gmail.com>
2024-12-22 20:30:22 +0300

    ddvdv

---
+++ lesson_2_dictionary.py
@@ -0,0 +1,127 @@
+obj = {
+    "key":30,
+    "255":"wffff",
+    "qwe":[1,2,4],
+    488:"wdw",
+}
+# print(obj)
+# print(obj["255"])
+
+
+# obj_arr = {
+#     0:1,
+#     1:30,
+#     2:50,
+#     3:300,
+# }
+# print(obj_arr[2])
+
+
+
+# obj.clear()
+# obj = ""
+# print(obj)
+
+
+
+# obj.copy()
+
+
+# obj_2 = obj
+# print("obj =" , obj)
+# print("obj_2 =" , obj_2)
+# obj.clear()
+# print("obj =" , obj)
+# print("obj_2 =" , obj_2)
+
+
+
+# obj["qwer"] = 9
+# print(obj)
+
+
+
+# print(obj["123"])
+# print(obj.get("key"))
+# print(obj.get("123"))
+
+
+
+# print(obj.setdefault("key"))
+# print(obj.setdefault("123"))
+# print(obj.setdefault("123" , 999))
+# print(obj.setdefault("key" , 999))
+
+# print(obj)
+
+
+
+# print(obj.pop("qwe"))
+# print(obj)
+
+
+
+# print(obj.popitem())
+# print(obj)
+
+
+
+# print(obj.keys())
+# print(list(obj.keys()))
+
+
+
+# print(obj.values())
+# print(list(obj.values()))
+
+
+
+# obj.update({
+#     0:23,
+#     "oos":5678907
+# })
+
+# print(obj)
+
+
+
+# print(obj.fromkeys([1,2,3,5] , 20))
+
+
+
+# for key , val in obj.items():
+#     print(key , "=" , val)
+
+
+
+
+# inp = input("=== ")
+# num = None
+# if(inp == "q"):
+#     num = 1
+# elif(inp == "w"):
+#     num = 2
+# elif(inp == "e"):
+#     num = 3
+# elif(inp == "r"):
+#     num = 4
+# elif(inp == "t"):
+#     num = 5
+# print(num)
+
+
+
+
+
+# obj_if = {
+#     "q":1,
+#     "w":2,
+#     "e":3,
+#     "r":4,
+#     "t":5,
+# }
+
+
+
+
+
